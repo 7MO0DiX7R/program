5c9dd18a3b222d74c0eed4c848981adbd5dbace3
Hundaol fufa <hundaol.bekele@elev.ga.ntig.se>
2022-08-31 10:51:25 +0200

    sup

---
+++ Frågessport.py
@@ -0,0 +1,3 @@
+name = (input("What is your name? "))
+print("Hello",name,"and welcome to this breaking bad quiz.")
+print("This quiz has 4 questions with 5 points for each answer, so now lets see how much you now mr.white and his story.")
---
+++ Loop.py
@@ -0,0 +1,9 @@
+for _ in range(10): 
+    print("hej")
+
+
+for _ in range(8):
+    print(_)
+
+for _ in range(13,37):
+    print(_)
