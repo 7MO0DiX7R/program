a84f03b8b60e7dce2ba2afe72a07cc68a3ba49c2
Hundaol fufa <hundaol.bekele@elev.ga.ntig.se>
2022-09-28 02:07:51 +0200

    inti

---
+++ Turtle.py
@@ -0,0 +1,5 @@
+import turtle
+arif = turtle.Turtle()
+arif.speed(1)
+arif.shape("square")
+
--- another one.py
+++ another one.py
@@ -1,3 +1,3 @@
-a = int(input("frist:" ))
-b= int(input("second:" ))
+a =int(input("frist:" ))
+b=int(input("second:" ))
 print(a+b)
