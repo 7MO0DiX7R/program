65d1e0d1443aabec45b08f1fb1aa1cf8a2c9ae40
Hundaol fufa <hundaol.bekele@elev.ga.ntig.se>
2022-12-04 20:27:55 +0100

    none

---
+++ If_else.py
@@ -0,0 +1,8 @@
+
+while True:
+    year= int(input("Vilket år vill du testa? "))
+    if year%4 == 0:
+        print("its a leap year")
+    else:
+        print("it aint")
+
--- Loop.py
+++ Loop.py
@@ -1,9 +1,2 @@
 for _ in range(10): 
-    print("hej")
-
-
-for _ in range(8):
-    print(_)
-
-for _ in range(13,37):
-    print(_)
+    print()
